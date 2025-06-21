27351da410084256eb0e5fd506c49253fe285113
B.Santhoshkumar <santhoshkumar@handletechlabs.com>
2025-06-21 13:59:43 +0530

    updates

--- src/resources/views/screenWidth/reportWindowSize.blade.php
+++ src/resources/views/screenWidth/reportWindowSize.blade.php
@@ -5,8 +5,11 @@
     if (isNaN(screenWidth_get) || screenWidth_get <= 0) {
         screenWidth_get = 0;
     }
-    // let screenWidth_auto_reload = {{ config('screenWidth.auto_reload') }} ? {{ config('screenWidth.auto_reload') }} : false;
-    let screenWidth_auto_reload = true;
+    console.log('screenWidth.auto_reload');
+    console.log({{ config('screenWidth.auto_reload') }} );
+
+    let screenWidth_auto_reload = {{ config('screenWidth.auto_reload') }} ? {{ config('screenWidth.auto_reload') }} : false;
+    // let screenWidth_auto_reload = true;
     let screenWidth_width = 0;
 
     function screenWidth_update_width() {
